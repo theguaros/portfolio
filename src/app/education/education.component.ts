5e454465a172ed0ed6a0977301f0c16706145913
Adrian Guevara <adrianjga96@gmail.com>
2020-07-05 12:56:09 -0400

    Education edited

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,19 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  education:any = [
+  titulo = "";
+  fecha = "";
+  descripcion = "";
+  education:any[] = [
     {
-      fecha: '',
-      titulo: '',
-      
+      titulo: 'Ing. en Informática',
+      fecha: '2012-2020',
+      descripcion: 'Graduado en la Universidad Centroccidental "Lisandro Alvarado", obtiendo el titulo universitario de Ingeniero en Informatica. '      
     },
-    {},
-    {},
-    {}
+    {
+      titulo: 'Introducción al Desarrollo Web',
+      fecha: '01/2017',
+      descripcion: 'Curso de Introducción al Desarrollo Web: HTML y CSS (1/2) emitido por Google Activate'
+    },
+    {
+      titulo: 'Pasantías Profesionales',
+      fecha: '05/2019',
+      descripcion: 'Desarrollo de un MVP (Producto Mínimo Viable), una aplicación móvil de servicios de transporte privado.'
+    },
+    {
+      titulo: 'IBM Cloud Essentials',
+      fecha: '06/2019',
+      descripcion: 'Curso de IBM - Cloud Essentials. Todo lo referente a la nube de IBM'
+    },
+    {
+      titulo: 'Curso Profesional de React',
+      fecha: '04/2020',
+      descripcion: 'Principios básicos de como implementar React emitido por CódigoFacilito'
+    },
+    {
+      titulo: 'Curso Profesional de Javascript',
+      fecha: '05/2020',
+      descripcion: 'Aplicación intermedia de Javascript emitido por CódigoFacilito'
+    }
+
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  educations(number:any){
+    console.log("aqiu")
+    if(number == 1){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }else if(number == 2){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }else if(number == 3){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }else if(number == 4){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }else if(number == 5){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }else if(number == 6){
+      this.titulo = this.education[number-1].titulo;
+      this.fecha = this.education[number-1].fecha;
+      this.descripcion = this.education[number-1].descripcion;
+    }
   }
 
 }
